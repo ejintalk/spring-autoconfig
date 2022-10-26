84e9e03c3f2748546999eecffc31a1af8a984995
latte <latteonterrace@gmail.com>
2022-10-26 16:30:58 +0900

    minus added

--- src/main/java/com/jirepos/common/CalcUtil.java
+++ src/main/java/com/jirepos/common/CalcUtil.java
@@ -4,4 +4,7 @@ public class CalcUtil {
     public long multiply(long num1, long num2) {
         return num1 * num2;
     }
+    public long minus(long num1, long num2) {
+        return num1 - num2;
+    }
 }
