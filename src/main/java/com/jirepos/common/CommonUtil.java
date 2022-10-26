e4b9bca46ec4e77c79d2de5afe4d3d5201e761fb
jirepos <jirepos@gmail.com>
2022-10-26 13:03:52 +0900

    FileUtil 생성 및 Utility 클래스 생성

---
+++ src/main/java/com/jirepos/common/CommonUtil.java
@@ -0,0 +1,7 @@
+package com.jirepos.common;
+
+public class CommonUtil {
+    public String append(String str1, String str2) {
+        return str1 + str2;
+    }
+}
---
+++ src/main/java/com/jirepos/common/FileUtil.java
@@ -0,0 +1,7 @@
+package com.jirepos.common;
+
+public class FileUtil {
+    public void saveFile(String fileName, String content) {
+        // save file
+    }
+}
